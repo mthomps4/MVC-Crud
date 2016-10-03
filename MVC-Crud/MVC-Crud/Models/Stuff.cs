2bf0dfb66ae2e9e8d88d212b2eea0bbbceb30224
Matt <matthew.thompson.a@gmail.com>
2016-10-03 18:41:57 -0400

    Added CRUD Model and Database Planets

---
+++ MVC-Crud/MVC-Crud/Models/Stuff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Crud.Models
+{
+    public class Planet 
+    {
+        public int PlanetID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PlanetContext : DbContext
+    {
+        public DbSet<Planet> Planets { get; set; }
+    }
+}
